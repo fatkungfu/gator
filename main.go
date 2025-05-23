a938f8ff9a7b6002e89ff37654f9f7ed41374b0b
Eric Bourcier-Walsh <fatkungfu@gmail.com>
2025-05-22 22:18:49 -0400

    Implement user registration and reset functionality, update database schema and queries

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/fatkungfu/gator/internal/config"
+	"github.com/fatkungfu/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,14 +22,26 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting database: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	// Create state and commands
-	programState := &state{cfg: &cfg}
+	programState := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 
 	// Register commands
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
 
 	// Ensure we have at least one command
 	if len(os.Args) < 2 {
